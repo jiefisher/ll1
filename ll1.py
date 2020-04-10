7a1e83b1a23dee296c335662045a733c574de286
jie.fang <jie.fang@aispeech.com>
2020-04-10 18:20:14 +0800

    ast update

--- ll1.py
+++ ll1.py
@@ -1,4 +1,16 @@
 import copy
+class AST:
+    pass
+class OP(AST):
+    def __init__(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+class NUM(AST):
+    def __init__(self,val):
+        self.val=val
+
 def is_terminal(x):
     if x.isupper():
         return False
@@ -85,7 +97,21 @@ def construct_ll1_table(first_set,follow_set,origin_expression):
                 for b in first_expb:
                     if True:
                         ll1_table[lhs][b]=lhs+'->'+rhs
-    print(ll1_table)
+    return ll1_table
+
+class Tree(object):
+    "Generic tree node."
+    def __init__(self, name='root', children=None):
+        self.name = name
+        self.children = []
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+    def __repr__(self):
+        return self.name
+    def add_child(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
 
 def main():
     lines='''E->TH
@@ -97,9 +123,8 @@ K->*FK
 K->/FK
 K->e
 F->-F
-F->1
-F->(E)'''
-
+F->(E)
+F->1'''
     elements=set()
     expression={}
     lines=lines.split('\n')
@@ -111,8 +136,121 @@ F->(E)'''
         expression[line[0]]+=line[1],
     origin_expression=copy.deepcopy(expression)
     first_set=construct_first(elements,expression)
-    print(origin_expression)
+    # print(origin_expression)
     follow_set=construct_follow(elements,expression,first_set)
-    print(''.join(list(follow_set['E'])))
-    construct_ll1_table(first_set,follow_set,origin_expression)
+    # print(''.join(list(follow_set['E'])))
+    ll1_table=construct_ll1_table(first_set,follow_set,origin_expression)
+    # ll1_table["H"][")"] = "H->e"
+    # ll1_table["K"][")"] = "K->e"
+    # ll1_table["K"]["#"] = "K->e"
+    print(ll1_table)
+    stack=["E"]
+    expr="(1+1)*(1+1)#"
+    i=0
+    st=""
+    treestack=[Tree("E")]
+    root=treestack[0]
+    while len(stack)>0:
+        token= expr[i]
+        symbol=stack[-1]
+        stack.pop()
+        node=treestack[-1]
+        treestack.pop()
+        if not symbol.isupper():
+            symbol=stack[-1]
+            stack.pop()
+            treestack.pop()
+        
+        st+=symbol
+        try:
+            e=ll1_table[symbol][token]
+        except :
+            e=ll1_table[symbol]["e"]
+        e=e.split("->")[1]
+        
+        if e[0]==expr[i]:
+            i+=1
+            stack+=list(e)[::-1]
+            for x in list(e):
+                node.children.append(Tree(x))
+            
+            treestack+=node.children
+            # st+="("+e[0]+")"
+        elif e!='e':
+            stack+=list(e)[::-1]
+            for x in list(e):
+                node.children.append(Tree(x))
+            
+            treestack+=node.children
+            print(node.children)
+            # print(stack,e)
+        elif e=='e':
+            print(symbol,expr[i])
+            if expr[i]==')':
+                i+=1
+    s=[root]
+
+    def preorder(node):
+        t=[]
+        if not node:
+            return node
+        
+        if len(node.children)>0:
+            for x in node.children:
+                
+                temp=preorder(x)
+                t+=temp
+        t.append(node.name)        
+        return t
+    t=preorder(root)
+    print(t)
+    as_line=[i for i in t if not i.isupper() and i!="(" and i!=")"]
+    print(as_line)
+    stack=as_line[::-1]
+    root=None
+    head=None
+    treestack=[]
+    while len(stack)>0:
+        s=stack[-1]
+        stack.pop()
+        node=OP(s)
+        if root==None:
+            
+            root=node
+            head=node
+            treestack.append(node)
+        else:
+            if not root.left:
+                root.left=node
+            if not root.right:
+                root.right=node
+            else:
+                root=treestack[-1]
+                treestack.pop()
+        if node.val in ["+","-","*","/"]:
+            root =node
+            treestack.append(node)
+    
+    def binpreorder(node):
+        t=[]
+        if not node:
+            return node
+         
+        if node.left:
+            temp = binpreorder(node.left)
+            t+=temp
+        t.append(node.val)
+        if node.right:
+            temp = binpreorder(node.right)
+            t+=temp
+               
+        return t
+    a= binpreorder(head)
+    print(a)
+
+    
+
+
+
+
 main()
